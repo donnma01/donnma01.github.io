4a8a14bf10fa6ace890d0df859ba7ff18152c7e7
donnma01 <donnma01@luther.edu>
2015-10-17 16:27:20 -0500

    Update ToDoScript.js

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,12 +1,12 @@
 addTask = function() {
-var priority = document.querySelector('#priority').option.className;
+//var priority = document.querySelector('#priority').option.className;
 var litext = document.createTextNode(document.querySelector('#taskenter').value);
 var listitem = document.createElement("li");
 var cbox = document.createElement("input");
 cbox.type = "checkbox";
 listitem.appendChild(cbox);
 listitem.appendChild(litext);
-listitem.className = priority;
+//listitem.className = priority;
 cbox.onclick = completeTask;
 var ulsection1 = document.querySelector('#unlist');
 ulsection1.appendChild(listitem);
