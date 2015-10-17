5c4b6c26e24dc9fa0f717fc74d14e8352c07c539
donnma01 <donnma01@luther.edu>
2015-10-16 19:40:41 -0500

    Update ToDoScript.js

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,5 +1,10 @@
 addTask = function() {
-var li = document.createElement("li");
+var litext = document.createTextNode(document.querySelector('#taskenter').value);
+var listitem = document.createElement("li");
+listitem.appendChild(litext);
+var ulsection1 = document.querySelector('#unlist');
+ulsection1.appendChild(listitem);
+
 
 
 }
